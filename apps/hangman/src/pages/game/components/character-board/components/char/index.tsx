977ca52d43cf3ac30aae0edc890f181386dae3ec
Christopher Igbojekwe <christopher.igbojekwe@gmail.com>
2021-05-17 13:08:59 -0400

    remainder changes

--- apps/hangman/src/pages/game/components/character-board/components/char/index.tsx
+++ apps/hangman/src/pages/game/components/character-board/components/char/index.tsx
@@ -1,5 +1,6 @@
 import * as gameSelectors from '@packages/sdk/resources/game/selectors'
-import React from 'react'
+import FadeIn from '~/components/fade-in'
+import React, { useState, useEffect } from 'react'
 import { StyledCharacterBoardChar } from '../../styles'
 import { useSelector } from '@packages/sdk'
 
@@ -9,17 +10,39 @@ interface Props {
 }
 
 export default function Char({ char, index }: Props): JSX.Element {
+  const [correctGuessCount, setCorrectGuessCount] = useState(0)
   const numGuesses = useSelector(gameSelectors.numGuesses)
   const status = useSelector(gameSelectors.status)
   const wordGuessed = useSelector(gameSelectors.wordGuessed)
+  const word = useSelector(gameSelectors.word)
   const wordGuessedIndex = useSelector(gameSelectors.wordGuessedIndex) // Needed to trigger state update
   const isCorrect = wordGuessed[index] == char
   const charDisplay = isCorrect || status != 'playing' ? char : ''
   const charStatus = 'playing'
+  const fade = correctGuessCount === 0 && status == 'playing'
+
+  useEffect(() => {
+    if (isCorrect) {
+      let temp = correctGuessCount
+      temp++
+      setTimeout(function () {
+        setCorrectGuessCount(temp)
+      }, 250)
+    }
+    if (numGuesses == 0) {
+      setCorrectGuessCount(0) //reset local count when new data is fetched
+    }
+  }, [numGuesses])
 
   return (
     <StyledCharacterBoardChar status={charStatus}>
-      {charDisplay}
+      {fade ? (
+        <FadeIn direction="up" distance={25}>
+          <div>{charDisplay}</div>
+        </FadeIn>
+      ) : (
+        charDisplay
+      )}
     </StyledCharacterBoardChar>
   )
 }
